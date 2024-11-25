9d58338d2dd04ea2318658cdd55da7fec8eae6b7
Holly-Kuo <h50733105@gmail.com>
2024-11-25 22:49:08 +0800

    commit1

---
+++ APP1/static/js/content.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const pageTitle = document.getElementById("page-title");
+  const urlParams = new URLSearchParams(window.location.search);
+  const title = urlParams.get("title");
+  //pageTitle.textContent = title || "分類內容";
+});
+
+
+function goBack() {
+  window.history.back();
+}
---
+++ APP1/static/js/index.js
@@ -0,0 +1,7 @@
+document.getElementById("start-recording").addEventListener("click", () => {
+  alert("錄音功能尚未實作，這裡可連接語音轉文字服務。");
+});
+
+document.getElementById("next-page").addEventListener("click", () => {
+  window.location.href = "../templates/class.html";
+});
