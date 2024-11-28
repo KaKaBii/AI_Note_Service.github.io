a668b470f9be37a614f714f227ae2dd430089e76
jerome956 <jerome956@gmail.com>
2024-11-28 15:36:13 +0800

    新增extention

---
+++ extention/yating-api/asr-samples/sample.py
@@ -0,0 +1,19 @@
+from ailabs_asr.streaming import StreamingClient
+
+def on_processing_sentence(message):
+  print(f'hello: {message["asr_sentence"]}')
+
+def on_final_sentence(message):
+  print(f'world: {message["asr_sentence"]}')
+
+ # initial Stream client with the key applied from Dev Console
+asr_client = StreamingClient(
+  key='key-applied-from-devconsole')
+
+# start Streaming with a wav file
+asr_client.start_streaming_wav(
+  pipeline='asr-zh-tw-std',
+  # verbose=True,
+  file='your-voice-file.wav', #remove 'file' to switch to streaming mode. 
+  on_processing_sentence=on_processing_sentence,
+  on_final_sentence=on_final_sentence)
